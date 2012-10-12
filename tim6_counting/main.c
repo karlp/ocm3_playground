149089d272a72f749e565fe0680dcabb9cebbbf0
Karl Palsson <karlp@tweak.net.au>
2012-10-12 00:53:58 +0000

    Initial playthings for libopencm3 examples.
    This time, using tim6 for counting in millisecs instead of systick.

---
+++ tim6_counting/main.c
@@ -0,0 +1,133 @@
+/*
+ * Karl Palsson, 2012 <karlp@tweak.net.au
+ */
+
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <libopencm3/stm32/f1/rcc.h>
+#include <libopencm3/stm32/f1/flash.h>
+#include <libopencm3/stm32/f1/gpio.h>
+#include <libopencm3/stm32/f1/dma.h>
+#include <libopencm3/stm32/nvic.h>
+#include <libopencm3/stm32/exti.h>
+#include <libopencm3/stm32/usart.h>
+#include <libopencm3/stm32/systick.h>
+#include <libopencm3/stm32/timer.h>
+
+#include "syscfg.h"
+#include "nastylog.h"
+
+__attribute__( ( always_inline ) ) static inline void __WFI(void)
+{
+  __asm volatile ("wfi");
+}
+
+#define LOG_TAG __FILE__
+#define DLOG(format, args...)         nastylog(LOG_TAG ":DEBUG", format, ## args)
+#define ILOG(format, args...)         nastylog(LOG_TAG ":INFO", format, ## args)
+#define WLOG(format, args...)         nastylog(LOG_TAG ":WARN", format, ## args)
+
+static struct state_t state;
+
+
+void clock_setup(void) {
+    rcc_clock_setup_in_hsi_out_24mhz();
+    /* Lots of things on all ports... */
+    rcc_peripheral_enable_clock(&RCC_APB2ENR, RCC_APB2ENR_IOPAEN);
+    rcc_peripheral_enable_clock(&RCC_APB2ENR, RCC_APB2ENR_IOPBEN);
+    rcc_peripheral_enable_clock(&RCC_APB2ENR, RCC_APB2ENR_IOPCEN);
+
+    rcc_peripheral_enable_clock(&RCC_APB1ENR, RCC_APB1ENR_USART2EN);
+    // oh, and dma!
+    rcc_peripheral_enable_clock(&RCC_AHBENR, RCC_AHBENR_DMA1EN);
+    // and timers...
+    rcc_peripheral_enable_clock(&RCC_APB1ENR, RCC_APB1ENR_TIM6EN);
+    /* Enable AFIO clock. */
+    rcc_peripheral_enable_clock(&RCC_APB2ENR, RCC_APB2ENR_AFIOEN);
+}
+
+void usart_enable_all_pins(void) {
+    gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_USART2_TX);
+    gpio_set_mode(GPIOA, GPIO_MODE_INPUT, GPIO_CNF_INPUT_FLOAT, GPIO_USART2_RX);
+}
+
+void usart_console_setup(void) {
+    usart_set_baudrate(USART_CONSOLE, 115200);
+    usart_set_databits(USART_CONSOLE, 8);
+    usart_set_stopbits(USART_CONSOLE, USART_STOPBITS_1);
+    usart_set_mode(USART_CONSOLE, USART_MODE_TX);
+    usart_set_parity(USART_CONSOLE, USART_PARITY_NONE);
+    usart_set_flow_control(USART_CONSOLE, USART_FLOWCONTROL_NONE);
+    usart_enable(USART_CONSOLE);
+}
+
+/**
+ * Use USART_CONSOLE as a console.
+ * @param file
+ * @param ptr
+ * @param len
+ * @return 
+ */
+int _write(int file, char *ptr, int len) {
+    int i;
+
+    if (file == STDOUT_FILENO || file == STDERR_FILENO) {
+        for (i = 0; i < len; i++) {
+            if (ptr[i] == '\n') {
+                usart_send_blocking(USART_CONSOLE, '\r');
+            }
+            usart_send_blocking(USART_CONSOLE, ptr[i]);
+        }
+        return i;
+    }
+    errno = EIO;
+    return -1;
+}
+
+void BUTTON_DISCO_USER_isr(void) {
+    exti_reset_request(BUTTON_DISCO_USER_EXTI);
+    // if rising, save timer count
+    // if falling, calc into state
+}
+
+static volatile int t6ovf = 0;
+void tim6_isr(void) {
+    TIM_SR(TIM6) &= ~TIM_SR_UIF;
+    if (t6ovf++ > 1000) {
+        ILOG("TICK %d\n", state.bitcount++);
+        t6ovf = 0;
+    }
+}
+
+void setup_tim6(void) {
+    
+    timer_reset(TIM6);
+    // 24Mhz / 10khz -1.
+    timer_set_prescaler(TIM6, 2399);  // 24Mhz/10000hz - 1
+    // 10khz for 10 ticks = 1 khz overflow = 1ms overflow interrupts
+    timer_set_period(TIM6, 10);
+    
+    nvic_enable_irq(NVIC_TIM6_IRQ);
+    //nvic_set_priority(NVIC_TIM&_IRQ, 1);
+    timer_enable_update_event(TIM6);  // default at reset!
+    timer_enable_irq(TIM6, TIM_DIER_UIE);
+    timer_enable_counter(TIM6);
+}
+
+int main(void) {
+
+    memset(&state, 0, sizeof (state));
+
+    clock_setup();
+    usart_enable_all_pins();
+    usart_console_setup();
+    setup_tim6();
+    while (1) {
+        //__WFI();  // This breaks texane/stlink badly!
+        ;
+    }
+
+    return 0;
+}
---
+++ tim6_counting/syscfg.h
@@ -0,0 +1,48 @@
+/* 
+ * General configuration of the device
+ * 
+ * Karl Palsson <karlp@tweak.net.au> 2012
+ */
+
+#ifndef SYSCFG_H
+#define	SYSCFG_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#include <libopencm3/stm32/f1/gpio.h>
+#include <libopencm3/stm32/nvic.h>
+#include <libopencm3/stm32/exti.h>
+#include <libopencm3/stm32/usart.h>
+
+
+#define USART_CONSOLE USART2
+#define USE_NASTYLOG 1
+
+    // Discovery board LED is PC8 (blue led)
+#define LED_DISCO_GREEN_PORT GPIOC
+#define LED_DISCO_GREEN_PIN GPIO9
+#define LED_DISCO_BLUE_PORT GPIOC
+#define LED_DISCO_BLUE_PIN GPIO8
+
+#define BUTTON_DISCO_USER_PORT GPIOA
+#define BUTTON_DISCO_USER_PIN GPIO0
+#define BUTTON_DISCO_USER_EXTI EXTI0
+#define BUTTON_DISCO_USER_isr exti0_isr
+#define BUTTON_DISCO_USER_NVIC NVIC_EXTI0_IRQ
+
+
+    struct state_t {
+        unsigned long last_blink_time;
+        unsigned long last_dht_time;
+        int bitcount;
+    };
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* SYSCFG_H */
+
