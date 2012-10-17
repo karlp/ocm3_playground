83116c36da7454c12be81c9459b1379fa3bffb52
Karl Palsson <karlp@tweak.net.au>
2012-10-17 22:42:27 +0000

    Finally a working oneshot timer?!
    Something to do with timer_reset?

---
+++ timerirqs/main.c
@@ -0,0 +1,153 @@
+/*
+ * Karl Palsson, 2012 <karlp@tweak.net.au
+ */
+
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <libopencm3/stm32/f1/rcc.h>
+#include <libopencm3/stm32/f1/flash.h>
+#include <libopencm3/stm32/f1/gpio.h>
+#include <libopencm3/stm32/f1/dma.h>
+#include <libopencm3/stm32/nvic.h>
+#include <libopencm3/stm32/exti.h>
+#include <libopencm3/stm32/usart.h>
+#include <libopencm3/stm32/systick.h>
+#include <libopencm3/stm32/timer.h>
+
+#include "syscfg.h"
+#include "nastylog.h"
+
+#define LOG_TAG __FILE__
+#define DLOG(format, args...)         nastylog(LOG_TAG ":DEBUG", format, ## args)
+#define ILOG(format, args...)         nastylog(LOG_TAG ":INFO", format, ## args)
+#define WLOG(format, args...)         nastylog(LOG_TAG ":WARN", format, ## args)
+
+static struct state_t volatile state;
+
+volatile uint64_t ksystick;
+
+uint64_t millis(void)
+{
+    return ksystick;
+}
+
+/**
+ * Busy loop for X ms USES INTERRUPTS
+ * @param ms
+ */
+void delay_ms(unsigned int ms) {
+    uint64_t now = millis();
+    while (millis() - ms < now) {
+        ;
+    }
+}
+
+void sys_tick_handler(void)
+{
+    ksystick++;
+    state.milliticks++;
+    if (state.milliticks >= 1000) {
+        state.milliticks = 0;
+        ILOG("Tick: %d: %d\n", state.seconds++, TIM7_CNT);
+    }
+}
+
+void clock_setup(void) {
+    rcc_clock_setup_in_hsi_out_24mhz();
+    rcc_peripheral_enable_clock(&RCC_APB2ENR, RCC_APB2ENR_IOPAEN);
+    rcc_peripheral_enable_clock(&RCC_APB2ENR, RCC_APB2ENR_IOPBEN);
+    rcc_peripheral_enable_clock(&RCC_APB2ENR, RCC_APB2ENR_IOPCEN);
+    rcc_peripheral_enable_clock(&RCC_APB1ENR, RCC_APB1ENR_USART2EN);
+    rcc_peripheral_enable_clock(&RCC_APB1ENR, RCC_APB1ENR_TIM7EN);
+    rcc_peripheral_enable_clock(&RCC_APB2ENR, RCC_APB2ENR_AFIOEN);
+}
+
+void setup_console(void) {
+    gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_USART2_TX);
+    gpio_set_mode(GPIOA, GPIO_MODE_INPUT, GPIO_CNF_INPUT_FLOAT, GPIO_USART2_RX);
+    usart_set_baudrate(USART_CONSOLE, 115200);
+    usart_set_databits(USART_CONSOLE, 8);
+    usart_set_stopbits(USART_CONSOLE, USART_STOPBITS_1);
+    usart_set_mode(USART_CONSOLE, USART_MODE_TX);
+    usart_set_parity(USART_CONSOLE, USART_PARITY_NONE);
+    usart_set_flow_control(USART_CONSOLE, USART_FLOWCONTROL_NONE);
+    usart_enable(USART_CONSOLE);
+}
+
+/**
+ * Use USART_CONSOLE as a console.
+ * @param file
+ * @param ptr
+ * @param len
+ * @return 
+ */
+int _write(int file, char *ptr, int len) {
+    int i;
+
+    if (file == STDOUT_FILENO || file == STDERR_FILENO) {
+        for (i = 0; i < len; i++) {
+            if (ptr[i] == '\n') {
+                usart_send_blocking(USART_CONSOLE, '\r');
+            }
+            usart_send_blocking(USART_CONSOLE, ptr[i]);
+        }
+        return i;
+    }
+    errno = EIO;
+    return -1;
+}
+
+void setup_tim7(void) {
+    state.timed_out = false;
+//    timer_reset(TIM7);
+    // 1ms ticks, 23999
+    // 1usec ticks, 23
+    nvic_enable_irq(NVIC_TIM7_IRQ);
+    TIM7_CNT = 1;
+    TIM7_PSC = 23999;
+    TIM7_ARR = 5000;
+    TIM7_DIER |= TIM_DIER_UIE;
+    TIM7_CR1 |= TIM_CR1_CEN;
+    ILOG("Starting oneshot!\n");
+}
+
+
+void tim7_isr(void) {
+    timer_clear_flag(TIM7, TIM_SR_UIF);
+    timer_disable_irq(TIM7, TIM_DIER_UIE);
+    timer_disable_counter(TIM7);
+    nvic_disable_irq(NVIC_TIM7_IRQ);
+    state.timed_out = true;
+}
+
+
+static void systick_setup(void) {
+    /* 24MHz / 8 => 3000000 counts per second. */
+    systick_set_clocksource(STK_CTRL_CLKSOURCE_AHB_DIV8);
+    /* 3000000/3000 = 1000 overflows per second - every 1ms one interrupt */
+    systick_set_reload(3000);
+    systick_interrupt_enable();
+    systick_counter_enable();
+}
+
+
+int main(void) {
+
+    clock_setup();
+    setup_console();
+    systick_setup();
+    while (1) {
+        if (state.seconds - state.last_start > 10) {
+            state.last_start = state.seconds;
+            setup_tim7();
+        }
+        if (state.timed_out) {
+            ILOG("Finished!\n");
+            state.timed_out = false;
+        }
+    }
+
+    return 0;
+}
---
+++ timerirqs/syscfg.h
@@ -0,0 +1,57 @@
+/* 
+ * General configuration of the device
+ * 
+ * Karl Palsson <karlp@tweak.net.au> 2012
+ */
+
+#ifndef SYSCFG_H
+#define	SYSCFG_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#include <libopencm3/stm32/f1/gpio.h>
+#include <libopencm3/stm32/nvic.h>
+#include <libopencm3/stm32/exti.h>
+#include <libopencm3/stm32/usart.h>
+
+
+#define USART_CONSOLE USART2
+#define USE_NASTYLOG 1
+
+    // Discovery board LED is PC8 (blue led)
+#define LED_DISCO_GREEN_PORT GPIOC
+#define LED_DISCO_GREEN_PIN GPIO9
+#define LED_DISCO_BLUE_PORT GPIOC
+#define LED_DISCO_BLUE_PIN GPIO8
+
+#define BUTTON_DISCO_USER_PORT GPIOA
+#define BUTTON_DISCO_USER_PIN GPIO0
+#define BUTTON_DISCO_USER_EXTI EXTI0
+#define BUTTON_DISCO_USER_isr exti0_isr
+#define BUTTON_DISCO_USER_NVIC NVIC_EXTI0_IRQ
+
+#define RHT_PORT GPIOB
+#define RHT_PIN GPIO9
+#define RHT_EXTI EXTI9
+#define RHT_isr exti9_5_isr
+#define RHT_NVIC NVIC_EXTI9_5_IRQ
+
+
+    struct state_t {
+        int seconds;
+        int last_start;
+        int bitcount;
+        u16 timings[100];  // more than 40*2
+        bool timed_out;
+        int milliticks;
+    };
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* SYSCFG_H */
+
